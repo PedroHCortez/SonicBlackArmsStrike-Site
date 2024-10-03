fcd2e33e820bda4e4cf0b26086be0cba4cdb05df
Pedro Cortez <pedrohcortezlopes@gmail.com>
2024-10-03 02:14:58 -0300

    Site da fangame de Sonic
    Site em sua versão bem inicial

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Sonic: Black Arms Strike</title>
+</head>
+<body>
+    <div class="topo-pagina">
+        <div class="icon"><a href="index.php"><img src="img/icon sonic.png" href="index.php" alt="Icon Sonic" width="150px" height="150px"></a></div>
+        <div class="topo-titulo"><img src="img/titulo sonic game.png" alt="titulo jogo" width="300px" height="300px"></div>
+    </div>
+
+    <div class="container-global">
+        <h1>Black Arms Strike</h1>
+        Coisas escritas para dar ao site conteudo para testes
+    </div>
+</body>
+</html>
