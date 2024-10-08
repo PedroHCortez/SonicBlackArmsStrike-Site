8c378e0c1aaf4fb60f611ff3c6bef9fa8e8b5407
Pedro Cortez <pedrohcortezlopes@gmail.com>
2024-10-08 11:40:09 -0300

    Atualizações feita em sala (08/10/24)
    Atualizações feita em sala no dia oito de outubro de dois mil e vinte e quatro

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<?php
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -14,13 +8,44 @@
 </head>
 <body>
     <div class="topo-pagina">
-        <div class="icon"><a href="index.php"><img src="img/icon sonic.png" href="index.php" alt="Icon Sonic" width="150px" height="150px"></a></div>
-        <div class="topo-titulo"><img src="img/titulo sonic game.png" alt="titulo jogo" width="300px" height="300px"></div>
+        <div class="icon"><a href="index.php"><img src="img/icon_sonic.png" href="index.php" alt="Icon Sonic" width="200px" height="170px"></a></div>
+        <div class="topo-titulo"><img src="img/titulo_sonic_game.png" alt="titulo jogo" width="270px" height="270px"></div>
+        <div class="sonic-topo"><a href="https://youtu.be/hT1rup7O_us?si=uhe2Q6WIBbBAuhUF"><img src="img/sonictopo.png" alt="sonic png" width="230px" height="230px"></a></div>
     </div>
 
     <div class="container-global">
-        <h1>Black Arms Strike</h1>
-        Coisas escritas para dar ao site conteudo para testes
+        <div class="sobre">
+            <div class="sobre-conteudo">
+                <h1 class="welcome">Bem-vindo a Sonic: Black Arms Strike!</h1>
+                <div class="txt-conteudo">
+                    <text class="">
+                        Sonic the Hedgehog: Black Arms Strike é uma nova e emocionante fangame do universo Sonic!
+                        <br>
+                        Embarque em uma épica aventura para salvar Mobius da invasão alienígena dos Black Arms.
+                        <br>
+                        Mergulhe em uma narrativa divertida e intrigante onde Sonic e seus amigos tentam impedir o ataque dos Black Arms enquanto tentam acabar mais uma vez com os planos malignos do Dr.Eggman.
+                        <br>
+                        Prepare-se para batalhas desafiadoras, onde cada movimento conta! Monte sua equipe com seus personagens favoritos do universo Sonic e utilize suas habilidades únicas para derrotar os inimigos.
+                        <br>
+                        <br>
+                        Características:
+
+                        Combate por turnos: Planeje cada movimento com cuidado e explore as fraquezas dos seus inimigos.
+                        História original: Uma nova aventura épica que se conecta com a história principal da franquia Sonic.
+                        Personagens icônicos: Jogue com seus personagens favoritos e descubra novas habilidades e poderes.
+                        Mundo aberto: Explore um vasto mundo e descubra novos desafios e segredos.
+                        Prepare-se para a batalha!
+
+                        Sonic the Hedgehog: Black Arms Strike é a fangame que todo fã de Sonic precisa experimentar. Junte-se à luta e salve Mobius!
+
+                        [Link para download/mais informações]
+
+                        #SonicTheHedgehog #BlackArmsStrike #Fangame #RPG #CombatePorTurnos
+                    </text>
+                </div>
+            </div>
+            <div class="sobre-backgroundimg"></div>
+         </div>
     </div>
 </body>
 </html>
