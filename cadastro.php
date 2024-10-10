bdee5a34ff5a6f74b4904d5bab03672d3eee6849
Pedro Cortez <pedrohcortezlopes@gmail.com>
2024-10-10 11:47:41 -0300

    Atualização (10/10/24)
    Atualizações no site feito em sala de aula, término da página index e começo da página de cadastro

--- cadastro.php
+++ cadastro.php
@@ -2,3 +2,77 @@
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Cadastre-se e jogue!</title>
+</head>
+<body>
+    <div>
+        <div class="topo-pagina">
+            <div class="icon"><a href="index.php"><img src="img/icon_sonic.png" href="index.php" alt="Icon Sonic" width="200px" height="170px"></a></div>
+            <div class="topo-titulo"><img src="img/titulo_sonic_game.png" alt="titulo jogo" width="270px" height="270px"></div>
+            <div class="sonic-topo"><a href="https://youtu.be/hT1rup7O_us?si=uhe2Q6WIBbBAuhUF"><img src="img/sonictopo.png" alt="sonic png" width="230px" height="230px"></a></div>
+        </div>
+        <div class="topobackgroundimg"></div>
+    </div>
+
+    <div class="container-global">
+        <div class="sobre">
+            <div class="sobre-conteudo">
+                <h1 class="welcome">Sobre o jogo!</h1>
+                <div class="txt-conteudo">
+                    <text>
+                        <br>
+                        Sonic the Hedgehog: Black Arms Strike é uma nova e emocionante fangame do universo Sonic!
+                        <br>
+                        Embarque em uma épica aventura para salvar Mobius da invasão alienígena dos Black Arms.
+                        <br>
+                        <br>
+                        Mergulhe em uma narrativa divertida e intrigante onde Sonic e seus amigos tentam impedir o ataque dos Black Arms enquanto tentam acabar mais uma vez com os planos malignos do Dr.Eggman.
+                        <br>
+                        Planeje cada movimento com cuidado e explore as fraquezas dos seus inimigos.
+                        <br>
+                        <br>
+                        <img class="sonicforcesimg" src="img/sonic-forces.jpg" alt="">
+                        <br>
+                        <br>
+                        Uma nova aventura épica com personagens originais e das histórias em quadrinhos.
+                        <br>
+                        Jogue com seus personagens favoritos e descubra novas habilidades e poderes.
+                        <br>
+                        <br>
+                        <img class="sonic-charactersimg" src="img/sonic_characters.jpg" alt="" width="1100px" height="840px">
+                        <br>
+                        <br>
+                        Prepare-se para a batalha!
+                        <br>
+                        Junte-se à luta e salve Mobius!
+                        <br>
+                        <br>
+                        <div class="cadastrar"><a href="cadastro.php"><button class="botao-cadastro"><h3>Cadastre-se agora!</h3></button></a></div>
+                        <br>
+                        <br>
+                    </text>
+                </div>
+            </div>
+            <div class="sobre-backgroundimg"></div>
+         </div>
+    </div>
+</body>
+
+<footer class="rodape">
+    <div class="rodape-container">
+        <main class="map">
+            <div class="titulo-rodapé"><img src="img/titulo_sonic_game.png" alt="" width="300px" height="265px"></div>
+            <p>&copy; Todos os direitos reservados</p>
+        </main>
+        <div class="silver-rodape"><a href="https://youtu.be/_UDVzVmk1NQ?si=ZniBCGP22kTI7CJl"><img src="img/silver-rodape.png" alt="" height="250px" width="300px"></a></div>
+    </div>
+</footer>
+
+</html>
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -7,40 +9,51 @@
     <title>Sonic: Black Arms Strike</title>
 </head>
 <body>
-    <div class="topo-pagina">
-        <div class="icon"><a href="index.php"><img src="img/icon_sonic.png" href="index.php" alt="Icon Sonic" width="200px" height="170px"></a></div>
-        <div class="topo-titulo"><img src="img/titulo_sonic_game.png" alt="titulo jogo" width="270px" height="270px"></div>
-        <div class="sonic-topo"><a href="https://youtu.be/hT1rup7O_us?si=uhe2Q6WIBbBAuhUF"><img src="img/sonictopo.png" alt="sonic png" width="230px" height="230px"></a></div>
+    <div>
+        <div class="topo-pagina">
+            <div class="icon"><a href="index.php"><img src="img/icon_sonic.png" href="index.php" alt="Icon Sonic" width="200px" height="170px"></a></div>
+            <div class="topo-titulo"><img src="img/titulo_sonic_game.png" alt="titulo jogo" width="270px" height="270px"></div>
+            <div class="sonic-topo"><a href="https://youtu.be/hT1rup7O_us?si=uhe2Q6WIBbBAuhUF"><img src="img/sonictopo.png" alt="sonic png" width="230px" height="230px"></a></div>
+        </div>
+        <div class="topobackgroundimg"></div>
     </div>
 
     <div class="container-global">
         <div class="sobre">
             <div class="sobre-conteudo">
-                <h1 class="welcome">Bem-vindo a Sonic: Black Arms Strike!</h1>
+                <h1 class="welcome">Sobre o jogo!</h1>
                 <div class="txt-conteudo">
-                    <text class="">
+                    <text>
+                        <br>
                         Sonic the Hedgehog: Black Arms Strike é uma nova e emocionante fangame do universo Sonic!
                         <br>
                         Embarque em uma épica aventura para salvar Mobius da invasão alienígena dos Black Arms.
                         <br>
+                        <br>
                         Mergulhe em uma narrativa divertida e intrigante onde Sonic e seus amigos tentam impedir o ataque dos Black Arms enquanto tentam acabar mais uma vez com os planos malignos do Dr.Eggman.
                         <br>
-                        Prepare-se para batalhas desafiadoras, onde cada movimento conta! Monte sua equipe com seus personagens favoritos do universo Sonic e utilize suas habilidades únicas para derrotar os inimigos.
+                        Planeje cada movimento com cuidado e explore as fraquezas dos seus inimigos.
                         <br>
                         <br>
-                        Características:
-
-                        Combate por turnos: Planeje cada movimento com cuidado e explore as fraquezas dos seus inimigos.
-                        História original: Uma nova aventura épica que se conecta com a história principal da franquia Sonic.
-                        Personagens icônicos: Jogue com seus personagens favoritos e descubra novas habilidades e poderes.
-                        Mundo aberto: Explore um vasto mundo e descubra novos desafios e segredos.
+                        <img class="sonicforcesimg" src="img/sonic-forces.jpg" alt="">
+                        <br>
+                        <br>
+                        Uma nova aventura épica com personagens originais e das histórias em quadrinhos.
+                        <br>
+                        Jogue com seus personagens favoritos e descubra novas habilidades e poderes.
+                        <br>
+                        <br>
+                        <img class="sonic-charactersimg" src="img/sonic_characters.jpg" alt="" width="1100px" height="840px">
+                        <br>
+                        <br>
                         Prepare-se para a batalha!
-
-                        Sonic the Hedgehog: Black Arms Strike é a fangame que todo fã de Sonic precisa experimentar. Junte-se à luta e salve Mobius!
-
-                        [Link para download/mais informações]
-
-                        #SonicTheHedgehog #BlackArmsStrike #Fangame #RPG #CombatePorTurnos
+                        <br>
+                        Junte-se à luta e salve Mobius!
+                        <br>
+                        <br>
+                        <div class="cadastrar"><a href="cadastro.php"><button class="botao-cadastro"><h3>Cadastre-se agora!</h3></button></a></div>
+                        <br>
+                        <br>
                     </text>
                 </div>
             </div>
@@ -48,4 +61,15 @@
          </div>
     </div>
 </body>
+
+<footer class="rodape">
+    <div class="rodape-container">
+        <main class="map">
+            <div class="titulo-rodapé"><img src="img/titulo_sonic_game.png" alt="" width="300px" height="265px"></div>
+            <p>&copy; Todos os direitos reservados</p>
+        </main>
+        <div class="silver-rodape"><a href="https://youtu.be/_UDVzVmk1NQ?si=ZniBCGP22kTI7CJl"><img src="img/silver-rodape.png" alt="" height="250px" width="300px"></a></div>
+    </div>
+</footer>
+
 </html>
